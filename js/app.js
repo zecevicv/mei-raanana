b0af2efdda3c92a9338ee2c472995496c4ace357
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-09-08 11:52:06 +0200

    [ADD] - General Page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 /* #Banner Accordion
   ======================================================= */
-if (window.innerWidth > 1024) {
-  const collapsible = document.querySelector('.banner .accordion .collapsible:first-child');
-  const collapse = collapsible.querySelector('.collapse');
+if (document.querySelector('.banner .accordion')) {
+  if (window.innerWidth > 1024) {
+    const collapsible = document.querySelector('.banner .accordion .collapsible:first-child');
+    const collapse = collapsible.querySelector('.collapse');
 
-  collapsible.classList.add('show');
-  collapse.style.height = 'auto';
+    collapsible.classList.add('show');
+    collapse.style.height = 'auto';
+  }
 }
 
 /* #Post Slider
@@ -59,39 +61,41 @@ if (document.querySelector('.banner .box-top .swiper')) {
 
 /* #Slideshow
   ======================================================= */
-(function () {
-
-  // we set the 'fx' class on the first image when the page loads
-  document.getElementById('slideshow').getElementsByTagName('img')[0].className = "fx";
-
-  // this calls the kenBurns function every 4 seconds
-  // you can increase or decrease this value to get different effects
-  window.setInterval(kenBurns, 6000);
-
-  // the third variable is to keep track of where we are in the loop
-  // if it is set to 1 (instead of 0) it is because the first image is styled when the page loads
-  var images = document.getElementById('slideshow').getElementsByTagName('img'),
-    numberOfImages = images.length,
-    i = 1;
-
-  function kenBurns() {
-    if (i == numberOfImages) {
-      i = 0;
+if (document.querySelector('#slideshow')) {
+  (function () {
+  
+    // we set the 'fx' class on the first image when the page loads
+    document.getElementById('slideshow').getElementsByTagName('img')[0].className = "fx";
+  
+    // this calls the kenBurns function every 4 seconds
+    // you can increase or decrease this value to get different effects
+    window.setInterval(kenBurns, 6000);
+  
+    // the third variable is to keep track of where we are in the loop
+    // if it is set to 1 (instead of 0) it is because the first image is styled when the page loads
+    var images = document.getElementById('slideshow').getElementsByTagName('img'),
+      numberOfImages = images.length,
+      i = 1;
+  
+    function kenBurns() {
+      if (i == numberOfImages) {
+        i = 0;
+      }
+      images[i].className = "fx";
+  
+      // we can't remove the class from the previous element or we'd get a bouncing effect so we clean up the one before last
+      // (there must be a smarter way to do this though)
+      if (i === 0) {
+        images[numberOfImages - 2].className = "";
+      }
+      if (i === 1) {
+        images[numberOfImages - 1].className = "";
+      }
+      if (i > 1) {
+        images[i - 2].className = "";
+      }
+      i++;
+  
     }
-    images[i].className = "fx";
-
-    // we can't remove the class from the previous element or we'd get a bouncing effect so we clean up the one before last
-    // (there must be a smarter way to do this though)
-    if (i === 0) {
-      images[numberOfImages - 2].className = "";
-    }
-    if (i === 1) {
-      images[numberOfImages - 1].className = "";
-    }
-    if (i > 1) {
-      images[i - 2].className = "";
-    }
-    i++;
-
-  }
-})();
+  })();
+}
